919711ba381c8429661888df4e1352bb80c45d90
Dario Sechi <dario.sechi.rnd@gmail.com>
2021-09-07 19:42:54 +0200

    Simplify appearance module and fix minor bugs

--- src/modules/appearance.ts
+++ src/modules/appearance.ts
@@ -13,15 +13,8 @@ class _ {
 
   onChangeTheme(theme: Theme): any {}
 
-  private onChangeThemeInternal(theme: Theme): boolean {
-    this.data.theme = theme
-    Logger.debug('Appearance', 'onChangeThemeInternal', `The theme has been set to ${theme}.`)
-
-    return this.onChangeTheme(theme)
-  }
-
-  async initialize(): Promise<boolean> {
-    this.onChangeTheme(this.themeByPrefersColorScheme)
+  initialize(): boolean {
+    this.setTheme(Theme.SYSTEM, false)
 
     if (Environment.isWindowDefined) {
       let get: boolean
@@ -29,56 +22,48 @@ class _ {
       get = LocalStorage.get(LocalStorageName.APPEARANCE, this.data)
       if (!get) return false
 
-      this.onChangeTheme(this.data.theme)
+      this.setTheme(this.data.theme, false)
     }
 
     return true
   }
 
-  async toggleTheme(): Promise<boolean> {
+  toggleTheme(): boolean {
     switch (this.data.theme) {
       case Theme.DARK:
-        return this.onChangeThemeInternal(Theme.LIGHT)
+        return this.setTheme(Theme.LIGHT)
       case Theme.LIGHT:
-        return this.onChangeThemeInternal(Theme.DARK)
+        return this.setTheme(Theme.DARK)
       case Theme.SYSTEM:
-        return this.onChangeThemeInternal(this.themeByPrefersColorScheme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+        return this.setTheme(this.themeByPrefersColorScheme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
     }
   }
 
-  async setTheme(theme: Theme): Promise<boolean> {
+  setTheme(theme: Theme, store: boolean = true): boolean {
+    this.data.theme = theme
+    Logger.debug('Appearance', 'setTheme', `The theme has been set to ${theme}.`)
+
     switch (theme) {
       case Theme.DARK:
       case Theme.LIGHT:
-        this.onChangeThemeInternal(theme)
+        this.onChangeTheme(theme)
         break
       case Theme.SYSTEM:
-        this.setThemeByPrefersColorScheme()
+        this.onChangeTheme(this.themeByPrefersColorScheme)
         break
     }
 
-    if (Environment.isWindowDefined) {
+    if (Environment.isWindowDefined && store) {
       return LocalStorage.set(LocalStorageName.APPEARANCE, this.data)
     }
 
     return true
   }
 
-  setThemeByPrefersColorScheme(): boolean {
-    if (this.isThemeSystem) {
-      this.data.theme = Theme.SYSTEM
-      Logger.debug('Appearance', 'onChangeThemeInternal', `The theme has been set to ${Theme.SYSTEM}.`)
-
-      this.onChangeTheme(this.themeByPrefersColorScheme)
-
-      return true
-    }
-
-    return false
-  }
-
   private registerThemeEventListener(): void {
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (v: MediaQueryListEvent) => this.setThemeByPrefersColorScheme())
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', (v: MediaQueryListEvent) => this.isThemeSystem && this.setTheme(Theme.SYSTEM, false))
   }
 
   get themeByPrefersColorScheme(): Theme {
@@ -86,11 +71,25 @@ class _ {
   }
 
   get isThemeDark(): boolean {
-    return this.data.theme === Theme.DARK
+    switch (this.data.theme) {
+      case Theme.DARK:
+        return true
+      case Theme.LIGHT:
+        return false
+      case Theme.SYSTEM:
+        return this.themeByPrefersColorScheme === Theme.DARK
+    }
   }
 
   get isThemeLight(): boolean {
-    return this.data.theme === Theme.LIGHT
+    switch (this.data.theme) {
+      case Theme.DARK:
+        return false
+      case Theme.LIGHT:
+        return true
+      case Theme.SYSTEM:
+        return this.themeByPrefersColorScheme === Theme.LIGHT
+    }
   }
 
   get isThemeSystem(): boolean {
