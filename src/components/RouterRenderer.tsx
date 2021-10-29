c1aeda15493aa38f2f92a6b650f275fbbd0c6a4f
Dario Sechi <dario.sechi.rnd@gmail.com>
2021-10-29 19:55:38 +0200

    Add back the RouterRenderer which was removed by accident

---
+++ src/components/RouterRenderer.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { RouterRendererProps } from '../definitions/props'
+
+/**
+ * Renders the matching component to the current route in the context.
+ *
+ * Usage:
+ *
+ * ```typescript
+ * import React from 'react'
+ * import { RouterRenderer } from '@queelag/react-core'
+ * import { useRouteNode } from 'react-router5'
+ *
+ * const routerMap = new Map()
+ * routerMap.set('AUTH', (props) => <div {...props} id='AUTH' />)
+ * routerMap.set('HOME', (props) => <div {...props} id='HOME' />)
+ *
+ * function App() {
+ *   const context = useContext
+ *
+ *   return <RouterRenderer context={useRouteNode('')} map={routerMap} />
+ * }
+ * ```
+ *
+ * @category Component
+ */
+export function RouterRenderer(props: RouterRendererProps) {
+  const component = { element: props.map.get(props.route) || props.fallback || (() => <div />) }
+  return <component.element />
+}
