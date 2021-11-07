4907105528a0e11160ff7f7cb5972d0035131971
Dario Sechi <dario.sechi.rnd@gmail.com>
2021-11-07 18:25:22 +0100

    Make return type of forwardRef generate less verbose props

--- src/modules/forward.ref.ts
+++ src/modules/forward.ref.ts
@@ -1,13 +1,13 @@
-import { ForwardedRef, forwardRef as _, PropsWithoutRef, ReactElement, RefAttributes } from 'react'
+import { ForwardedRef, forwardRef as _, ReactElement } from 'react'
 
 type ForwardRefRenderFunction<T, P = {}> = (props: P, ref: ForwardedRef<T>) => ReactElement<P>
 type NullableForwardRefRenderFunction<T, P = {}> = (props: P, ref: ForwardedRef<T>) => ReactElement<P> | null
 
-type ForwardRefExoticComponent<P> = (props: P) => ReactElement<P>
-type NullableForwardRefExoticComponent<P> = (props: P) => ReactElement<P> | null
+type ForwardRefExoticComponent<T, P = {}> = (props: P) => ReactElement
+type NullableForwardRefExoticComponent<T, P = {}> = (props: P) => ReactElement | null
 
-function forwardRef<T, P = {}>(render: ForwardRefRenderFunction<T, P>): ForwardRefExoticComponent<PropsWithoutRef<P> & RefAttributes<T>>
-function forwardRef<T, P = {}>(render: NullableForwardRefRenderFunction<T, P>): NullableForwardRefExoticComponent<PropsWithoutRef<P> & RefAttributes<T>>
+function forwardRef<T, P = {}>(render: ForwardRefRenderFunction<T, P>): ForwardRefExoticComponent<T, P>
+function forwardRef<T, P = {}>(render: NullableForwardRefRenderFunction<T, P>): NullableForwardRefExoticComponent<T, P>
 function forwardRef(...args: any[]) {
   return _(args[0]) as any
 }
